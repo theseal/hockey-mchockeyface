c3f22ba94c47720d993eff80afe7ff5dd23d767f
Johan Wassberg <johan@rymdvarel.se>
2023-07-21 20:10:48 +0200

    Major rewrite of python

--- hockey-mchockeyface.py
+++ hockey-mchockeyface.py
@@ -1,10 +1,31 @@
-#/usr/bin/env python3
+#!/usr/bin/env python3
 
-from flask import Flask
-from flask import render_template
+import logging
+import urllib.parse
+
+from flask import Flask, Response, render_template, request
+
+from hockeyface import hockeyface
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
+hf = hockeyface()
 
-@app.route('/')
+
+@app.route("/")
 def index_page():
-    return render_template('index.html')
+    return render_template("index.html")
+
+
+@app.route("/calendar")
+def calendar():
+    teams = request.args.getlist("team")
+    events = hf.get_events(teams)
+    ical = hf.build_ical(events, teams)
+
+    response = Response(response=ical, status=200, mimetype="text/calendar")
+    response.headers["Content-disposition"] = "attachment; filename=hockey-mchockeyface.ics"
+
+    return response
