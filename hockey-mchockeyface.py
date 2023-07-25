edd8779e1c4e44198a12d19b9faa95ed212aaa55
Johan Wassberg <johan@rymdvarel.se>
2023-07-25 19:27:36 +0200

    Redirect older clients
    E.g from the heruko domain

--- hockey-mchockeyface.py
+++ hockey-mchockeyface.py
@@ -2,10 +2,13 @@
 
 import logging
 import os
+from urllib.parse import urlparse
 
-from flask import Flask, Response, render_template, request, send_from_directory
+from flask import Flask, Response, redirect, render_template, request, send_from_directory
 
 from hockeyface import hockeyface
+
+redirect_to = os.environ.get("REDIRECT_TO")
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -14,18 +17,40 @@ app = Flask(__name__)
 hf = hockeyface()
 
 
+def ensure_domain(request):
+    if redirect_to:
+        logger.debug(f"Redirect to {redirect_to} is configured")
+        requested_domain = urlparse(request.url).netloc
+        path = urlparse(request.url).path
+        query = urlparse(request.url).query
+        if requested_domain != redirect_to:
+            logger.debug(f"Creating direct from {requested_domain}")
+            return redirect(f"https://{redirect_to}{path}?{query}", code=301)
+
+
 @app.route("/")
 def index_page():
+    _redirect = ensure_domain(request)
+    if _redirect:
+        return _redirect
+
     return render_template("index.html")
 
 
 @app.route("/favicon.ico")
 def favicon():
+    _redirect = ensure_domain(request)
+    if _redirect:
+        return _redirect
     return send_from_directory(os.path.join(app.root_path, "static"), "images/noun_55243_cc.png", mimetype="image/png")
 
 
 @app.route("/calendar")
 def calendar():
+    _redirect = ensure_domain(request)
+    if _redirect:
+        return _redirect
+
     teams = request.args.getlist("team")
     events = hf.get_events(teams)
     ical = hf.build_ical(events, teams)
