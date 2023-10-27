104b11d628801615be6a1687877db9ab99941400
Johan Wassberg <johan@rymdvarel.se>
2023-10-27 20:47:05 +0200

    First attempt of RSS feed

--- hockey-mchockeyface.py
+++ hockey-mchockeyface.py
@@ -67,3 +67,49 @@ def calendar():
     response.headers["Content-disposition"] = "attachment; filename=hockey-mchockeyface.ics"
 
     return response
+
+
+@app.route("/feed")
+def feed():
+    import requests
+    from feedgen.feed import FeedGenerator
+
+    team_store = {
+        "LIF": {
+            "url": "https://www.leksandsif.se/api/articles/site-news/list?pagesize=15",
+            "name": "Leksands IF",
+            "title": "leksandsif.se",
+            "href": "https://www.leksandsif.se",
+            "desc": "Länkar till ariklar på Leksandsif.se",
+        }
+    }
+    team = request.args.get("team")
+    labels_to_skip = request.args.getlist("labelToSkip")
+
+    url = team_store[team]["url"]
+
+    if team not in team_store:
+        response = Response(status=400)
+        return response
+
+    fg = FeedGenerator()
+    fg.id(f"Hockeyface {team}")
+    fg.link(href=team_store[team]["href"])
+    fg.title(team_store[team]["title"])
+    fg.description(team_store[team]["desc"])
+
+    r = requests.get(url)
+    returned_json = r.json()
+    for article in reversed(returned_json["data"]["articleItems"]):
+        if "label" in article["metadata"]:
+            if article["metadata"]["label"] in labels_to_skip:
+                continue
+        fe = fg.add_entry()
+        fe.id(article["id"])
+        fe.title(article["header"])
+        fe.link(href=f"{team_store[team]['href']}/article/{article['id']}/view")
+
+    response = Response(
+        response=fg.rss_str(pretty=True), status=200, mimetype="text/calendar"
+    )
+    return response
